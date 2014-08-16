3838e44bf6e8d3e5472f073764f3b64eab026ba3
Vazgen <vazkus@yahoo.com>
2014-08-15 22:03:40 -0700

    Changed the test. Instead of driving a single buzzer it now controls 2 LEDs. The buzzer test is now in a different tester.

--- test_pwm/main.cpp
+++ test_pwm/main.cpp
@@ -16,27 +16,39 @@ int main(void)
 {
 
     PWMBase* p = PWM<2>::instance();
-    PWMBase::Channel* ch = new PWMBase::Channel();
-    ch->gpio_pin = 7;
-    ch->gpiote_channel = 2;
-    ch->ppi_channels[0] = 0;
-    ch->ppi_channels[1] = 1;
-    ch->target_value = 100;
 
+    PWMBase::Channel* ch0 = new PWMBase::Channel();
+    ch0->gpio_pin = 3;
+    ch0->gpiote_channel = 2;
+    ch0->ppi_channels[0] = 0;
+    ch0->ppi_channels[1] = 1;
+    ch0->target_value = 100;
 
-    p->add_channel(ch);
+    PWMBase::Channel* ch1 = new PWMBase::Channel();
+    ch1->gpio_pin = 5;
+    ch1->gpiote_channel = 1;
+    ch1->ppi_channels[0] = 2;
+    ch1->ppi_channels[1] = 3;
+    ch1->target_value = 100;
+
+    p->add_channel(ch0);
+    p->add_channel(ch1);
     p->initialize();
 
-    const float FREQ_HALF_NOTE_FACTOR = 1.059463f;
-    float frequency = 440.0f;
-
+    int duty = 0;
+    int val = 100;
+    int change = 1;
     while(true) {
-        p->set_channel_value(0, (16000000+(frequency/2))/frequency, 50);
-        frequency *= FREQ_HALF_NOTE_FACTOR;
-        if(frequency > (440.0f * 4.0f)) {
-            frequency = 440.0f;
-        }
-        nrf_delay_us(500000);
+
+        duty += change;
+        if(duty == 100) change = -1;
+        else
+        if(duty == 0) change = 1;
+
+        p->set_channel_value(0, val, duty);
+        p->set_channel_value(1, val, 100-duty);
+
+        nrf_delay_ms(20);
     }
 
     return 0;
